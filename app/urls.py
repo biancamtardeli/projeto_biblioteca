14e2d8e14bdce22683267168301288f456f007c1
Bianca Martins Tardeli <10794815677@MUZAMBINHO.EDU.BR>
2024-12-03 16:13:53 -0300

    frontend

--- app/urls.py
+++ app/urls.py
@@ -2,5 +2,13 @@ from django.urls import include, path
 from .views import *
 
 urlpatterns = [
-    path("", IndexView.as_view(), name="index"),
+    path('', IndexView.as_view(), name='index'),
+    path('autor/', AutorView.as_view(), name='autor'),
+    path('cidade/', CidadeView.as_view(), name='cidade'),
+    path('editora/', EditoraView.as_view(), name='editora'),
+    path('emprestimo/', EmprestimoView.as_view(), name='emprestimo'),
+    path('genero/', GeneroView.as_view(), name='genero'),
+    path('livro/', LivroView.as_view(), name='livro'),
+    path('uf/', UfView.as_view(), name='uf'),
+    path('usuario/', UsuarioView.as_view(), name='usuario'),
 ]
--- app/views.py
+++ app/views.py
@@ -4,8 +4,87 @@ from .models import *
 
 class IndexView(View):
     def get(self, request):
-        produtos = UF.objects.all()
-        return render(request, 'index.html', {'produtos': produtos})
+      page = "index.html"
+      return render(request, page)
     def post(self, request):
-        pass
-# Create your views here.F
+       pass
+
+class AutorView(View):
+    def get(self, request):
+      page = "autor.html"
+      context = {
+        "autores": Autor.objects.all()
+      }
+      return render(request, page, context)
+    def post(self, request):
+       pass
+    
+class UfView(View):
+    def get(self, request):
+      page = "uf.html"
+      context = {
+        "ufs": UF.objects.all()
+      }
+      return render(request, page, context)
+    def post(self, request):
+       pass
+
+class CidadeView(View):
+    def get(self, request):
+      page = "cidade.html"
+      context = {
+        "cidades": Cidade.objects.all()
+      }
+      return render(request, page, context)
+    def post(self, request):
+       pass
+
+class EditoraView(View):
+    def get(self, request):
+      page = "editora.html"
+      context = {
+        "editoras": Editora.objects.all()
+      }
+      return render(request, page, context)
+    def post(self, request):
+       pass
+      
+class GeneroView(View):
+    def get(self, request):
+      page = "genero.html"
+      context = {
+        "generos": Genero.objects.all()
+      }
+      return render(request, page, context)
+    def post(self, request):
+       pass
+
+class LivroView(View):
+    def get(self, request):
+      page = "livro.html"
+      context = {
+        "livros": Livro.objects.all()
+      }
+      return render(request, page, context)
+    def post(self, request):
+       pass
+    
+class UsuarioView(View):
+    def get(self, request):
+      page = "usuario.html"
+      context = {
+        "usuarios": Usuario.objects.all()
+      }
+      return render(request, page, context)
+    def post(self, request):
+       pass
+
+class EmprestimoView(View):
+    def get(self, request):
+      page = "emprestimo.html"
+      context = {
+        "emprestimos": Emprestimo.objects.all()
+      }
+      return render(request, page, context)
+    def post(self, request):
+       pass
